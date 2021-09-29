4430ba30dc360734354b261bff4330db9768739b
arnavgupta03 <harshulgupta05@gmail.com>
2021-09-28 21:34:35 -0400

    added dialogue for events

--- src/events.rs
+++ src/events.rs
@@ -8,3 +8,15 @@ pub struct Event {
     pub date: DateTime,
     pub todo: Vec,
 }
+
+pub fn events(path: &String) {
+    let choices = vec!["create", "edit", "view", "delete"];
+    let choice = dialoguer::Select::new()
+        .with_prompt("What would you like to do?")
+        .items(choices)
+        .default(0)
+        .interact()
+        .unwrap();
+
+    let choice = choices[choice].to_string();
+}
